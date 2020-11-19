0cfa2cd431d91338c080bab68a71b68a489f721a
Ian Walls <wallsianh@gmail.com>
2020-11-19 15:25:08 -0500

    [changed] extend to extended urlencoded

--- wikistack.js
+++ wikistack.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 
 app.use(morgan('dev'));
-app.use(express.urlencoded({extend: false}));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json);
 app.use(express.static(path.join(__dirname, 'public')));
 
