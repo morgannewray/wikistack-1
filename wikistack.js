82e08d3dfd096589d5102ce550ee6dd6eff7efea
Ian Walls <wallsianh@gmail.com>
2020-11-19 16:06:01 -0500

    [fixed] body parser

--- wikistack.js
+++ wikistack.js
@@ -3,16 +3,21 @@ const express = require('express');
 const pg = require('pg');
 const app = express();
 const path = require('path');
+const { addPage, editPage, main, userList, userPages, wikiPage } = require('./views'); //this means it looks directly at index.js
+const layout = require('./views/layout');
 
 app.use(morgan('dev'));
+//Body Parser
 app.use(express.urlencoded({extended: false}));
-app.use(express.json);
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  console.log('Hi');
+  res.send(layout('Hello World!'))
 })
 
-app.listen(1587, () => {
+app.listen(3000, () => {
   console.log('This is working!');
 })
