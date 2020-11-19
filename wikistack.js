68f7cea2a670f1c78f74afe364a54c62985a1b50
Ian Walls <wallsianh@gmail.com>
2020-11-19 13:28:34 -0500

    [started] wikistack project

---
+++ wikistack.js
@@ -0,0 +1,18 @@
+const morgan = require('morgan');
+const express = require('express');
+const pg = require('pg');
+const app = express();
+const path = require('path');
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extend: false}));
+app.use(express.json);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(1587, () => {
+  console.log('This is working!');
+})
